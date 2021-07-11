c16798bae2c64d9a14170a9d95e32666de6d22c1
I-Dohyun-I <trouna43@gmail.com>
2021-07-11 16:49:22 +0900

    add function

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ bot = commands.Bot(command_prefix='>', status=discord.Status.online, activity=ga
 SCHUL_INFO = {}
 
 
+
 @bot.command(aliases=['도움말'])
 async def info(ctx):
     embed = discord.Embed(title="School Bot", description="유니폭스와 함께 만드는 학교봇", color=0x4432a8)
@@ -24,14 +25,30 @@ async def school(ctx, name):
 
 @bot.command(aliases=['급식정보'])
 async def lunch(ctx, name):
-    info_request(name)
-    url = "https://open.neis.go.kr/hub/mealServiceDietInfo?Type=Json&SD_SCHUL_CODE="+SCHUL_INFO[name][1]
-    url += "&ATPT_OFCDC_SC_CODE="+SCHUL_INFO[name][2]+"&MLSV_YMD=20210709&KEY=<API KEY>"
+    info = info_request(name)
+    url = "https://open.neis.go.kr/hub/mealServiceDietInfo?Type=Json&SD_SCHUL_CODE="+info[1]
+    url += "&ATPT_OFCDC_SC_CODE="+info[2]+"&MLSV_YMD=20210709&KEY=<API KEY>"
     #print(url)
     data = requests.get(url).json()
     data = data['mealServiceDietInfo'][1]['row'][0]['DDISH_NM'].replace("<br/>","\n")
     return await ctx.send(data)
 
+@bot.command(aliases=['시간표'])
+async def timetable(ctx, name, date):
+    #https://open.neis.go.kr/hub/misTimetable?ATPT_OFCDC_SC_CODE=T10&SD_SCHUL_CODE=7003714 
+    table = ""
+    info = info_request(name)
+    url = "https://open.neis.go.kr/hub/hisTimetable?SD_SCHUL_CODE="+info[1]+"&ATPT_OFCDC_SC_CODE="+info[2]+"&GRADE=2&CLASS_NM=3&ALL_TI_YMD="+date+"&Type=json&KEY=<API KEY>&SCHUL_NM="
+    data = requests.get(url).json()
+    data = data['hisTimetable']
+    for i in range(len(data[1]['row'])):
+        table += f"{i+1}교시 : {data[1]['row'][i]['ITRT_CNTNT']}\n" #data[1]['row'][i]['ITRT_CNTNT']+"\n"
+    return await ctx.send(table)
+
+@bot.command(aliases=['학사일정'])
+async def school(ctx, name):
+    data = info_request(name)
+    return await ctx.send(data)
 
 def info_request(name):
     if SCHUL_INFO.get(name) == None:
@@ -45,4 +62,4 @@ def info_request(name):
         return SCHUL_INFO[name]
 
 
-bot.run('<TOKEN>')
+bot.run('<discord token>')
